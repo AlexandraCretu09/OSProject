46093da31b98cd6a1236226c3b1e723f732b4120
AlexandraCretu09 <alexabianca091@gmail.com>
2023-04-27 14:36:37 +0300

    Checked whether the 1st arg is a file, dir or link

--- Source.c
+++ Source.c
@@ -7,6 +7,24 @@
 #include <unistd.h>
 #include <sys/stat.h>
 
-int main(){
-    printf("Yes.\n");
+int main(int argc, char** argv){
+    if(argc<2){
+        printf("There aren't any arguments!\n");
+        exit(0);
+    }
+
+    struct stat var;
+    if(stat(argv[1], &var) == -1){
+        perror("stat");
+        exit(0);
+    }
+
+    if(S_ISREG(var.st_mode)){
+        printf("%s is a regular file\n", argv[1]);
+    } else if(S_ISDIR(var.st_mode)){
+        printf("%s is a directory\n",argv[1]);
+    } else if(S_ISLNK(var.st_mode)){
+        printf("%s is a sybmolic link\n",argv[1]);
+    }
+    return 0;
 }
